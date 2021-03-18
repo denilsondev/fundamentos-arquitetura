c5efefe23bb25eed7e816d88a929899849f94735
denilsondev <denilsoncdc10@gmail.com>
2021-03-17 21:41:59 -0300

    doc: Conceitos de estado e comportamento

---
+++ OOP/02 - Pilares OOP/01 - EstadoComportamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Pessoa
+    {
+        //Estados são as propriedades da classe;
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        //comportamentos são metodos da classe que processam uma informação, que devolve informação ou que alteram o estado da classe;
+        public int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+
+            if (dataAtual < DataNascimento.AddYears(idade)) idade--;
+            return idade;
+        }
+    }
+}
