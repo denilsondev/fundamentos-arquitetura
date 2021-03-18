65ced5ee26d3ce902ab5e8414c0e436739ce4486
denilsondev <denilsoncdc10@gmail.com>
2021-03-17 22:05:35 -0300

    doc: Herança

---
+++ OOP/02 - Pilares OOP/02 - Heranca.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP._02___Pilares_OOP
+{
+    // Herança é um meio de voce herdar todos as propriedades e métodos da classe mãe
+    // Classe base "É uma" classe mãe. Ex: Funcionario É UMA Pessoa. Se isso nao fizer sentido, significa que não deveria ter este tipo de herança. Nunca isso podera ser pensado na estrategia de economizar implementação.
+    class Funcionario : Pessoa 
+    {
+        public DateTime DataAdmissao { get; set; }
+        public int Registro { get; set; }
+    }
+
+    public class Processo
+    {
+        public void Execucao()
+        {
+            var funconario = new Funcionario()
+            {
+                Nome = "Joao da silva",
+                DataNascimento = Convert.ToDateTime("1990/01/01"),
+                DataAdmissao = DateTime.Now,
+                Registro = 1234
+            };
+        }
+    }
+}
