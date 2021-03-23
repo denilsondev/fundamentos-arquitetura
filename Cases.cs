350aeadc427787daf2713efcf5ebb2f1b1e4f35b
denilsondev <denilsoncdc10@gmail.com>
2021-03-22 22:05:37 -0300

    foc: demo interface vs imp

---
+++ Cases.cs
@@ -0,0 +1,74 @@
+namespace OOP
+{
+    public interface IRepositorio
+    {
+        void Adicionar();
+    }
+
+
+    public class Repositorio : IRepositorio
+    {
+        public Repositorio(int a)
+        {
+
+        }
+
+        public void Adicionar()
+        {
+
+        }
+
+    }
+
+    public class RepositorioFake : IRepositorio
+    {
+        public void Adicionar()
+        {
+            int teste;
+            teste = "e";
+        }
+
+    }
+
+    public class UsoImplementacao
+    {
+        public void Processo()
+        {
+            var repositorio = new Repositorio();
+            repositorio.Adicionar();
+        }
+    }
+
+    public class UsoAbstracao
+    {
+        private readonly IRepositorio _repositorio;
+
+        public UsoAbstracao(IRepositorio repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        public void Processo()
+        {
+            _repositorio.Adicionar()
+        }
+    }
+
+    public class TesteInterfaceImImplementacao
+    {
+        public TesteInterfaceImImplementacao()
+        {
+            var repoImp = new UsoImplementacao();
+            repoImp.Processo();
+
+            var repoAbs = new UsoImplementacao(new Repositorio());
+            repoAbs.Processo();
+
+            var repoAbsFake = new UsoAbstracao(new RepositorioFake());
+            repoAbsFake.Processo();
+                
+        }
+    }
+
+
+
