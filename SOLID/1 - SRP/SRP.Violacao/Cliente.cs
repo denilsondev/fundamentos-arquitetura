4ea70ed8a767c88ecca9c5eef1f25135fee6eaa4
denilsondev <denilsoncdc10@gmail.com>
2021-03-30 21:58:56 -0300

    doc: Solid - SRP violation

---
+++ SOLID/1 - SRP/SRP.Violacao/Cliente.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Mail;
+using System.Text;
+
+namespace SOLID
+{
+    //Single Responsability Principle Violation - Esta classe tem varios motivos de mudanca. Tanto no contexto de validacao, conexao, query, email, etc.
+    class Cliente
+    {
+        public  int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public string AdicionarCliente()
+        {
+            if (!Email.Contains("@"))
+                return "Cliente com e-mail inválido";
+
+            if (CPF.Length != 11)
+                return "Cliente com CPF inválido";
+
+
+            using(var cn = new SqlConnection())
+            {
+                var cmd = new SqlCommand();
+
+                cn.ConnectionString = "MinhaConnectionString";
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT INTO CLIENTE (NOME, EMAIL) VALUES (@nome, @email)";
+
+                cmd.Parameters.AddWithValue("nome", Nome);
+                cmd.Parameters.AddWithValue("email", Email);
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            var mail = new MailMessage("empresa@empresa.com", Email);
+            var client = new SmtpClient
+            {
+                Port = 25,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Host = "smtp.google.com"
+            };
+
+            mail.Subject = "Bem vindo";
+            mail.Body = "Parabens! Voce esta cadastrado";
+            client.Send(mail);
+
+            return "Cliente cadastrado com sucesso";
+
+        }
+
+    }
+}
