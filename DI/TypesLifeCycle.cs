8bdbae82a503a096c058083faf243083e8809b4f
denilsondev <denilsoncdc10@gmail.com>
2021-04-14 17:17:15 -0300

    doc tipos de ciclo de vida de injecao de dependencia

---
+++ DI/TypesLifeCycle.cs
@@ -0,0 +1,16 @@
+namespace DI {
+    class TypesLifeCycle {
+
+        //Transient 
+        //Transient lifetime services are created each time they`re requested from the service container. This lifetime works best for lightweight stateless services
+
+
+        //Scoped
+        //Scoped lifetimes services are created once per cliente request (connect) - One creation per request
+
+        //Singleton
+        //Created the first time they are requested (or when configureServices is run and an instance specified with service registration).
+
+    }
+
+}
